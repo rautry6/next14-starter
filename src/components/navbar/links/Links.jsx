3af51138e5bd3436c33667a052c34818a6360aa8
rautry6 <94204738+rautry6@users.noreply.github.com>
2024-03-20 17:05:23 -0400

    Admin + Login/Logout button Added

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -22,11 +22,25 @@ const Component = () => {
         },
     ]
 
-    return(
+    //TEMPORARY
+    const session = true;
+    const isAdmin = true;
+
+    return (
         <div className={styles.links}>
-            {links.map((link) =>(
-                <NavLink item={link} key={link.title}/>
+            {links.map((link) => (
+                <NavLink item={link} key={link.title} />
             ))}
+            {session ? (
+                    <>
+                        {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+
+                ) : (
+                    <NavLink item={{ title: "Login", path: "/login" }} />
+                )
+            }
         </div>
     )
 }
