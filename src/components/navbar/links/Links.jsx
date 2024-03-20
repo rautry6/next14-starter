1864754b87acf56fd625380d6143a49904bafbbe
rautry6 <94204738+rautry6@users.noreply.github.com>
2024-03-20 17:28:28 -0400

    Reactive Navar
    Navbar grows and shrinks based on screen size. Changes to menu dropdown when screen is too small.

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,37 +1,42 @@
-import Link from "next/link";
+"use client"
+
 import styles from "./links.module.css"
 import NavLink from "./navLink/NavLink";
+import { useState } from "react";
 
-const Component = () => {
-    const links = [
-        {
-            title: "Homepage",
-            path: "/",
-        },
-        {
-            title: "About",
-            path: "/about",
-        },
-        {
-            title: "Contact",
-            path: "/contact",
-        },
-        {
-            title: "Blog",
-            path: "/blog",
-        },
-    ]
+const links = [
+    {
+        title: "Homepage",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/about",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+]
+
+const Links = () => {
+    const [open, setOpen] = useState(false);
 
     //TEMPORARY
     const session = true;
     const isAdmin = true;
 
     return (
-        <div className={styles.links}>
-            {links.map((link) => (
-                <NavLink item={link} key={link.title} />
-            ))}
-            {session ? (
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.title} />
+                ))}
+                {session ? (
                     <>
                         {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
                         <button className={styles.logout}>Logout</button>
@@ -40,9 +45,19 @@ const Component = () => {
                 ) : (
                     <NavLink item={{ title: "Login", path: "/login" }} />
                 )
+                }
+            </div>
+            <button className={styles.menuButton} onClick={()=>setOpen((prev) => !prev)}>Menu</button>
+            {
+                open && (<div className={styles.mobileLinks}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.title} />
+                    ))}
+                </div>
+                )
             }
         </div>
     )
 }
 
-export default Component;
+export default Links;
